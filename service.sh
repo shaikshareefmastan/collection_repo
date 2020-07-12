ac5a6f2b8566c7401efc97bdfb55ae7303ee0c97
EC2 Default User <ec2-user@ip-172-31-41-46.ap-south-1.compute.internal>
2020-07-12 09:29:58 +0000

    dhcsjv

--- service.sh
+++ service.sh
@@ -1,4 +1,3 @@
 #/bin/bash
-url=`kubectl get service -l app=apiservice | awk '{ print $4 }' | sed -n '2 p'`
-echo "$url"
+`kubectl get service -l app=apiservice | awk '{ print $4 }' | sed -n '2 p'` > /tmp/aa
 
